4b859fb1d7e83a8bc2d5190d6de7726bb0181f1a
Baris <karalarbaris@gmail.com>
2021-06-12 08:32:43 +0200

    Initial Commit

---
+++ Project-4-BetterRest-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-4-BetterRest-SwiftUI
+//
+//  Created by Baris Karalar on 12.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-4-BetterRest-SwiftUI/Project_4_BetterRest_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_4_BetterRest_SwiftUIApp.swift
+//  Project-4-BetterRest-SwiftUI
+//
+//  Created by Baris Karalar on 12.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_4_BetterRest_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
