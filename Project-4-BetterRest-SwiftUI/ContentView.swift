eeccd82a1695e8730a2527f8cf720d12cd59120d
Baris <karalarbaris@gmail.com>
2021-06-13 13:01:31 +0200

    Cleaning up the user interface

--- Project-4-BetterRest-SwiftUI/ContentView.swift
+++ Project-4-BetterRest-SwiftUI/ContentView.swift
@@ -10,37 +10,56 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date()
+    @State private var wakeUp = defaultWakeTime
     @State private var coffeeAmount = 1
     
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 5
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+    }
+    
     var body: some View {
         
         NavigationView {
-            VStack {
-                Text("When would you like to wake up?")
-                    .font(.headline)
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                    Text("\(sleepAmount, specifier: "%g") hours")
+            Form {
+                VStack (alignment: .leading, spacing:0) {
+                    Text("When would you like to wake up?")
+                        .font(.headline)
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
                 }
                 
-                Text("Daily coffee intake")
-                    .font(.headline)
-                Stepper(value: $coffeeAmount, in: 0...20) {
-                    if coffeeAmount == 1 {
-                        Text("\(coffeeAmount) cup")
-                    } else {
-                        Text("\(coffeeAmount) cups")
+                VStack (alignment: .leading, spacing:0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
                     }
                 }
+                
+                VStack (alignment: .leading, spacing:0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    Stepper(value: $coffeeAmount, in: 0...20) {
+                        if coffeeAmount == 1 {
+                            Text("\(coffeeAmount) cup")
+                        } else {
+                            Text("\(coffeeAmount) cups")
+                        }
+                    }
+                }
+                
+                
+                
+                
+                
                 
             }
             .navigationBarTitle("BetterRest")
@@ -53,7 +72,7 @@ struct ContentView: View {
                 Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("Okay")))
             }
         }
-
+        
         
         
         
